d54480561a62401199bf6f2f054fc11eaaf25201
@AS <andrei.sandu@computerminds.co.uk>
2025-05-09 23:58:51 +0100

    removed unused declaration

--- src/app/components/Cart.tsx
+++ src/app/components/Cart.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
 import Image from "next/image";
 import { useCart } from "./CartContext";
